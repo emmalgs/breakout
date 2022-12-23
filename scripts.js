86149c784f8d634a35ab28571eba9fa2f31eb37a
Emma Gerigscott <elgerig@gmail.com>
2022-12-23 10:28:52 -0500

    Block class

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+
+class Block {
+    constructor(xAxis, yAxis)  {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
 
 //draw the block
 function addBlock() {
