6ba03fce472d941ae027b942540926dfddc0d758
Emma Gerigscott <elgerig@gmail.com>
2022-12-23 10:26:02 -0500

    function addBlock

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 const grid = document.querySelector('.grid')
 
+//draw the block
+function addBlock() {
 const block = document.createElement('div')
 block.classList.add('block')
+block.style.left = '100px'
+block.style.bottom = '50px'
 grid.append(block)
+}
+
+addBlock()
