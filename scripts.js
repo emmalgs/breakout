b4dbb7155f741c0d9c10a32f3d0e902c5f97dfb1
Emma Gerigscott <elgerig@gmail.com>
2022-12-23 14:59:08 -0500

    move ball

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
+const boardWidth = 560
 
 const userStart = [230, 10]
 let currentPosition = userStart
+
+const ballStart = [230, 40]
+let ballCurrentPosition = ballStart
 
 class Block {
     constructor(xAxis, yAxis)  {
@@ -44,7 +48,7 @@ function addBlocks() {
         block.classList.add('block')
         block.style.left = blocks[i].bottomLeft[0] + 'px'
         block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-        grid.append(block)
+        grid.appendChild(block)
     }
 }
 
@@ -53,6 +57,52 @@ addBlocks()
 //add user
 const user = document.createElement('div')
 user.classList.add('user')
-user.style.left = currentPosition[0] +'px'
-user.style.bottom = currentPosition[1] + 'px'
+drawUser()
 grid.appendChild(user)
+
+//draw the user
+function drawUser() {
+    user.style.left = currentPosition[0] +'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
+
+//draw the ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+//move user
+function moveUser(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+            currentPosition[0] -= 10
+            drawUser()
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10
+                drawUser()
+                }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += 2
+    ballCurrentPosition[1] += 2
+    drawBall()
+}
+
+setInterval(moveBall, 30)
+
