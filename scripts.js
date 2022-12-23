2c4d9ec29764bbd9781d04044dc428f17f546fb8
Emma Gerigscott <elgerig@gmail.com>
2022-12-23 10:39:16 -0500

    3 rows of blocks

--- scripts.js
+++ scripts.js
@@ -11,13 +11,40 @@ class Block {
     }
 }
 
-//draw the block
-function addBlock() {
-const block = document.createElement('div')
-block.classList.add('block')
-block.style.left = '100px'
-block.style.bottom = '50px'
-grid.append(block)
+//all the blocks
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210)
+]
+
+console.log(blocks[0])
+
+//draw all the block
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.append(block)
+    }
 }
 
-addBlock()
+addBlocks()
+
+//add user
